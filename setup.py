d663aceb09d8d1d2737d00bdd76789fd98af4071
mattia allegro <mattia.allegro@yahoo.it>
2023-06-02 15:35:57 +0200

    tests 0.0.5

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="django-iubenda-web",
     packages=["django_iubenda"],
-    version="0.0.4",
+    version="0.0.5",
     license="Iubenda integration for web application",
     description="Iubenda integration for web application",
     author="ShadMod",
     author_email="support@shadmod.it",
     url="https://github.com/shadMod/django_iubenda",
-    download_url="https://github.com/shadMod/django_iubenda/archive/refs/tags/0.0.4.tar.gz",
+    download_url="https://github.com/shadMod/django_iubenda/archive/refs/tags/0.0.5.tar.gz",
     keywords=[
         "Django iubenda",
     ],
