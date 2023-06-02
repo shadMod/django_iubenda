072cd4e508bd67a0c9a5b35fb1207d3c3151c127
mattia allegro <mattia.allegro@yahoo.it>
2023-06-02 14:48:18 +0200

    init setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name="django_iubenda",
+    packages=["django_iubenda"],
+    version="0.1",
+    license="Iubenda integration for web application",
+    description="Iubenda integration for web application",
+    author="ShadMod",
+    author_email="support@shadmod.it",
+    url="https://github.com/shadMod/django_iubenda",
+    download_url="https://github.com/shadMod/django_iubenda/archive/refs/tags/0.0.1.tar.gz",
+    keywords=[
+        "WebPwn3r",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
